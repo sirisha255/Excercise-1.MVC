e8753bb7f146d5c2f5f4fde10da368578eff4489
Sirisha <Sirisha@DESKTOP-856P7A7>
2022-11-05 19:21:08 +0100

    guessing game added

---
+++ Excercise-1.MVC/Controllers/GuessingGame.cs
@@ -0,0 +1,29 @@
+﻿using Excercise_1.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Excercise_1.MVC.Controllers
+{
+    public class GuessingGame : Controller
+    {
+        [HttpPost]
+        public IActionResult GuessNumber(int guess)
+        {
+            if (guess != 0)
+            {
+                ViewBag.Number = Models.Guess.RandomNumbers(guess);
+                return View();
+            }
+            else
+            {
+                ViewBag.Number = "Please enter a number";
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult GuessNumber()
+        {
+            return View();
+        }
+    }
+}
+
+
---
+++ Excercise-1.MVC/Models/Guess.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Excercise_1.MVC.Models
+{
+    public class Guess
+    {
+        public static string RandomNumbers(int guess)
+        {
+            Random random = new();
+            int rand = random.Next(0, 10);
+            int guesses = 0;
+            guess = Convert.ToInt32(guess);
+            if (guess < rand)
+            {
+                guesses++;
+                return "The guessing number " + guess + " was to small and the rand nr is " + rand + " You has guessed " + guesses + "times.";
+            }
+            else if (guess > rand)
+            {
+                guesses++;
+                return "The guessing number " + guess + " was to big " + rand + " You has guessed " + guesses + "times.";
+            }
+            else if (guess == rand)
+            {
+                return guess + " Your guesses is equals " + rand + " and you have guesses " + guesses + " times";
+            }
+            return "Over and out";
+
+        }
+    }
+
+}
